ce2d838296594ae78183a64d9366195ea9fb8a13
Yash <ypatodkar@gmail.com>
2022-01-30 14:39:46 +0530

    First commit

---
+++ D1Q1.java
@@ -0,0 +1,24 @@
+import java.util.*;
+class D1Q1
+{
+	public static void main(String[] ars)
+	{
+		Scanner sc = new Scanner(System.in);
+		if(sc.hasNextDouble())
+		{
+			//double a=sc.nextDouble();
+			//System.out.println("Number"+a);
+	
+			System.out.println("The Average is "+
+			(sc.nextDouble()+sc.nextDouble())/2);
+			System.out.println("Enter");
+	
+			//double a=sc.nextDouble();
+			//System.out.println("Number"+a);
+	
+		}
+		else
+			System.out.println("Err");
+	}
+	//sc.close();
+/usr/bin/bash: wq: command not found
